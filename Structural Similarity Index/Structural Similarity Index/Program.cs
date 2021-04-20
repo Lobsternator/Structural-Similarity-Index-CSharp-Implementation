9a1cd390bd47483edc67e46af8c9dad3bb5c332a
Lobsternator <31416401+Lobsternator@users.noreply.github.com>
2021-04-20 17:36:07 +0200

    Add files via upload

---
+++ Structural Similarity Index/Structural Similarity Index/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralSimilarityIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var img1 = new Bitmap("Images/1.png");
+            var img2 = new Bitmap("Images/2.png");
+            var comparer = new Comparer(img1, img2, 64, 8);
+            var similarity = comparer.CalculateSimilarity();
+
+            Console.WriteLine($"Image 1 and image 2 have a {similarity * 100}% similarity.");
+            Console.Read();
+        }
+    }
+}
