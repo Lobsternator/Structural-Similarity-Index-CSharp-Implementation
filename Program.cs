75377b176e32f289a421facc63b38d53035c19c9
Julius Edvardsson <juliusensen@gmail.com>
2021-11-10 22:36:09 +0100

    Tidied up the repo to make it less cluttered

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace StructuralSimilarityIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+                return;
+
+            Bitmap   img1       = new Bitmap(args[0]);
+            Bitmap   img2       = new Bitmap(args[1]);
+            Comparer comparer   = new Comparer(img1, img2, 64, PixelFormat.Format24bppRgb, 8);
+            double   similarity = comparer.CalculateSimilarity();
+
+            Console.WriteLine($"Image A and image B have a {similarity * 100}% similarity.");
+            Console.Read();
+        }
+    }
+}
